b106fb54854378f87e5bf3c5b782ffd8947ca307
Aaron Osteraas <aaron.osteraas@gmail.com>
2019-06-05 12:55:01 +1000

    how good's git?

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # scomo
+
+How good's a readme?
+
+## Have a yarn
+
+People that have a yarn, get a yarn, to claim your yarn: `yarn add scomo`.
+
+## Using Scomo
+
+```js
+import Scomo from 'scomo';
+
+function HaveAGo(props) {
+  return <Scomo imgsrc="path/to/image.png" />;
+}
+```
--- src/index.js
+++ src/index.js
@@ -1,22 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+const style = {
+  position: 'fixed',
+  right: 0,
+  bottom: 0
+};
+
+const talkStyle = {
+  position: 'relative',
+  padding: '1.6rem',
+  background: '#404040',
+  color: '#f9f9f9',
+  borderRadius: '0.5575rem'
+};
 
 export default function Scomo(props) {
-  const style = {
-    position: 'fixed',
-    right: 0,
-    bottom: 0
-  };
-  const talkStyle = {
-    position: 'relative',
-    padding: '1.6rem',
-    background: '#404040',
-    color: '#f9f9f9',
-    borderRadius: '0.5575rem'
-  };
+  const { imgsrc, imgwidth, imgalt } = props;
+
+  const [that, setThat] = useState('that');
+
+  useEffect(() => {
+    document.addEventListener('mouseup', handleThat);
+    return () => document.removeEventListener('mouseup');
+  });
+
+  function handleThat() {
+    let selection = document.getSelection
+      ? document.getSelection().toString()
+      : document.selection.createRange().toString();
+    setThat(selection);
+  }
+
   return (
     <div style={style}>
-      <div style={talkStyle}>How good's that?</div>
-      <img src={props.imgsrc} width={props.imgwidth || 320} alt="asd" />
+      <div style={talkStyle}>How good's {that}?</div>
+      <img src={imgsrc} width={imgwidth || 320} alt={imgalt} />
     </div>
   );
 }
