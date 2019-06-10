2326af01d2a28bba518f87cf64e43ebeb7677942
Aaron Osteraas <aaron.osteraas@gmail.com>
2019-06-11 08:36:31 +1000

    give scotty some zest

--- src/index.js
+++ src/index.js
@@ -3,7 +3,15 @@ import React, { useState, useEffect } from 'react';
 const style = {
   position: 'fixed',
   right: 0,
-  bottom: 0
+  bottom: -110
+};
+
+const getStyle = that => {
+  const bottom = that ? 0 : -110;
+  return {
+    ...style,
+    bottom
+  };
 };
 
 const talkStyle = {
@@ -14,10 +22,27 @@ const talkStyle = {
   borderRadius: '0.5575rem'
 };
 
+const getTalkStyle = that => {
+  const display = that ? 'block' : 'none';
+  return {
+    ...talkStyle,
+    display
+  };
+};
+
+const closeStyle = {
+  position: `absolute`,
+  right: `0.6rem`,
+  top: `50%`,
+  transform: `translateY(-50%)`,
+  fontWeight: `bolder`,
+  cursor: `pointer`
+};
+
 export default function Scomo(props) {
   const { imgsrc, imgwidth, imgalt } = props;
 
-  const [that, setThat] = useState('that');
+  const [that, setThat] = useState('');
 
   useEffect(() => {
     document.addEventListener('mouseup', handleThat);
@@ -32,8 +57,13 @@ export default function Scomo(props) {
   }
 
   return (
-    <div style={style}>
-      <div style={talkStyle}>How good's {that}?</div>
+    <div style={getStyle(that)}>
+      <div style={getTalkStyle(that)}>
+        How good's {that}?{' '}
+        <div style={closeStyle} onClick={() => setThat('')}>
+          X
+        </div>{' '}
+      </div>
       <img src={imgsrc} width={imgwidth || 320} alt={imgalt} />
     </div>
   );
